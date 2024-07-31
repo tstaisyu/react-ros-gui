1951ebd0b6a2054d96a03f3caa78cac83cc78eac
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-07-31 20:15:09 +0900

    Add license

--- src/contexts/ROSContext.js
+++ src/contexts/ROSContext.js
@@ -14,11 +14,3 @@
  * limitations under the License.
  */
 
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
